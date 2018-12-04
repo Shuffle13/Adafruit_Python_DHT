842260dd7606acb211b432e8d97b8c71d4e26457
lkk644877ukk <lkk644877ukk@gmail.com>
2018-12-04 19:23:12 +0800

    AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -44,30 +44,30 @@ if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
     pin = sys.argv[2]
 else:
     print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
-    print('Example:sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connect$
+    print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
     sys.exit(1)
 def post_to_mcs(payload):
         headers = {"Content-type": "application/json", "deviceKey": deviceKey}
         not_connected = 1
         while (not_connected):
                 try:
-                        httpClient = httplib.HTTPConnection("api.mediatek.com:8$
+                        httpClient = httplib.HTTPConnection("api.mediatek.com:80")
                         httpClient.connect()
                         not_connected = 0
                 except (httplib.HTTPException, socket.error) as ex:
                         print ("Error: %s" % ex)
 			time.sleep(10)  # sleep 10 seconds
-        httpClient.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints$
+        httpClient.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers)
         response = httpClient.getresponse()
-        print( response.status, response.reason, json.dumps(payload), time.strf$
+        print( response.status, response.reason, json.dumps(payload), time.strftime("%c"))
         data = response.read()
         httpClient.close()
 
 while True:
         h0,t0= Adafruit_DHT.read_retry(sensor, pin)
         if h0 is not None and t0 is not None:
-	print('Temp={0:0.1f}*Humidity={1:0.1f}%'.format(t0, h0))
-                payload = {"datapoints":[{"dataChnId":"Humidity","values":{"val$
+		print('Temp={0:0.1f}*Humidity={1:0.1f}%'.format(t0,h0))
+                payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
                 {"dataChnId":"Temperature","values":{"value":t0}}]}
                 post_to_mcs(payload)
                 time.sleep(10)
